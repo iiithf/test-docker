9aaf31a9b169f8146dc7b9e90302475fd8c240c3
Subhajit Sahu <wolfram77@gmail.com>
2019-03-15 22:06:13 +0530

    pre smoke test

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.7-slim
+
+WORKDIR /app
+COPY . /app
+
+RUN pip install --trusted-host pypi.python.org -r requirements.txt
+
+EXPOSE 80
+ENV NAME World
+CMD [ "python3", "app.py" ]
---
+++ app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from flask import Flask
+from redis import Redis, RedisError
+import socket
+import os
+
+
+db = Redis(host='redis', db=0, socket_connect_timeout=2, socket_timeout=2)
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+  try:
+    visits = db.incr('counter')
+  except RedisError:
+    visits = '<i>cannot connect to redis, counter disabled</i>'
+  html = \
+    '<h3>Hello {name}!</h3>' \
+    '<b>Hostname:</b> {hostname}<br>' \
+    '<b>Visits:</b> {visits}'
+  return html.format(name=os.getenv('NAME', 'world'), hostname=socket.gethostbyname(), visits=visits)
+
+
+if __name__ == '__main__':
+  app.run(host='', port=80)
